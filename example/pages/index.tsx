3a9c373e2f1c19d9dddb6b0108f394bb49759364
Henrique <riquessan@gmail.com>
2021-05-18 19:15:46 -0300

    Example fix

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import NextImageMagnify from "../../index";
+import NextImageMagnify from "../components/NextImageMagnify";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
